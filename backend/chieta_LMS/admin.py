985b832dcb7fbb35b69fa4d0464a9402a73c3cce
Blessing Mbalaka <bjmbalaka@gmail.com>
2025-05-23 09:10:56 +0200

    Add Django backend folder with project files

---
+++ backend/chieta_LMS/admin.py
@@ -0,0 +1,6 @@
+Generating admin.py stub with comments... 
+from django.contrib import admin 
+from .models import CustomUser  # replace with your model name 
+ECHO is off.
+# Register your models here 
+admin.site.register(CustomUser) 
