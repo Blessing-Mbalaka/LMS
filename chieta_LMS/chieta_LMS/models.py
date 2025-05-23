e12535a7395114a446ba541a9081e5993c867a32
Blessing Mbalaka <bjmbalaka@gmail.com>
2025-05-23 10:34:40 +0200

    Add Django backend folder with project files

--- chieta_LMS/chieta_LMS/models.py
+++ chieta_LMS/chieta_LMS/models.py
@@ -1,9 +1,11 @@
 from django.db import models 
 from django.contrib.auth.models import AbstractUser 
-ECHO is off.
-# Uncomment to extend the default user model: 
-# class CustomUser(AbstractUser): 
-#     # add extra fields here 
-#     pass 
-ECHO is off.
-# Remember to set AUTH\_USER\_MODEL in settings.py if extending 
+from django.db import models
+from django.contrib.auth.models import User
+from google import genai
+from docx import Document
+import fitz  # PyMuPDf
+
+
+# Gemini API key
+genai_client = genai.Client(api_key="AIzaSyB2IyI2KVqDLexX2AdqPMP6aeja23aZCKw")
