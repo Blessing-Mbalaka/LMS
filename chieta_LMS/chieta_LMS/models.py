8d373a2ba5f49483bfec93cc9cd68b05e038b0f1
Blessing Mbalaka <bjmbalaka@gmail.com>
2025-05-23 08:53:05 +0200

    Import Django project files

---
+++ chieta_LMS/chieta_LMS/models.py
@@ -0,0 +1,9 @@
+from django.db import models 
+from django.contrib.auth.models import AbstractUser 
+ECHO is off.
+# Uncomment to extend the default user model: 
+# class CustomUser(AbstractUser): 
+#     # add extra fields here 
+#     pass 
+ECHO is off.
+# Remember to set AUTH\_USER\_MODEL in settings.py if extending 
