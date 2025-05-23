b6bab00904ea6c67cb79b6ae15723d9adca6c5fc
Blessing Mbalaka <bjmbalaka@gmail.com>
2025-05-23 10:44:27 +0200

    Add Django backend folder with project files

--- chieta_LMS/chieta_LMS/urls.py
+++ chieta_LMS/chieta_LMS/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import generate_paper
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("generate-paper/", generate_paper, name="generate_paper"),
+    path("chieta/", include("chieta_lms.urls")),
+
 ]
